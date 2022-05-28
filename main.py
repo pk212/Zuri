bd73ad7b2fb0e6af630c955d6fea7220fb3e6d2f
Muazzam <66058056+pk212@users.noreply.github.com>
2022-05-28 23:23:03 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
-# Read text from a file, and count the occurence of words in that text
-# Example:
-# count_words("The cake is done. It is a big cake!") 
-# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.track = tracks
+        self.score = score
+        print("My name is", name, "I am", age, "my tracks are", tracks, "and I scored", score)
 
-from click import open_file
-
-
-def read_file_content(filename):
-    # [assignment] Add your code here 
-    with open("C:/Users/NCC/Desktop/Web Dev/Zuri Python/Reading-Text-Files/story.txt", "r") as openfile:
-        read_file_content = openfile.read()
-    return read_file_content
-        # print(read_file_content)
- 
+Bob = Student(name = "Bob", age = 26, tracks = ["FE","BE"],score = 20.90)
 
 
-def count_words():
-    text = read_file_content("C:/Users/NCC/Desktop/Web Dev/Zuri Python/Reading-Text-Files/story.txt")
-    # [assignment] Add your code here
-    
-    split_text = text.split()
-    # print(split_text)
-    count = {}
-    for i in split_text:
-        if i in count:
-            count[i] += 1
-        else:
-             count[i] = 1
-    return count
-print(count_words())
+
+s = Student("Peter", 34, ["UI", "UX"], 40.9)
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
